f0f79cfdbdca734d6afeaa6a09c1b72135d479b0
june.kim <june.kim@kakaocorp.com>
2017-09-12 01:18:30 +0900

    add .gitignore. fix files

--- files.py
+++ files.py
@@ -18,5 +18,23 @@ print('There were', count, 'subject lines in', fname)
 # fname = input("Enter file name: ")
 fname = "mbox.txt"
 fh = open(fname)
-data = fh.read()
-print(data.upper().strip())
+
+mode = "rstrip"
+
+if mode == "read":
+    data = fh.read()
+    print(data.upper().strip())
+else:
+    for line in fh:
+        print(line.rstrip().upper())
+
+
+fname = "mbox-short.txt"
+fh = open(fname)
+sum = 0.0
+count = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    sum = sum + float(line[line.find(":")+1:])
+    count = count + 1
+print("Average spam confidence: ", sum/count)
