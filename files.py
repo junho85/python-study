ddc0023582fa294319ac0d1a5970aedcdd25b88b
june.kim <june.kim@kakaocorp.com>
2017-09-12 00:55:11 +0900

    add files

---
+++ files.py
@@ -0,0 +1,16 @@
+fhand = open('mbox-short.txt')
+for line in fhand:
+    line = line.rstrip()
+    if not line.startswith('From:'):
+        continue
+    print(line)
+
+# fname = input('Enter the file name: ')
+# fname = 'mbox-short.txt'
+fname = 'mbox.txt'
+fhand = open(fname)
+count = 0
+for line in fhand:
+    if line.startswith('Subject:'):
+        count = count + 1
+print('There were', count, 'subject lines in', fname)
