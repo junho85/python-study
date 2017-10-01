b52b19e8100628e165363f1dbf30c99d4d4c4728
june.kim <junho85@gmail.com>
2017-10-01 15:08:18 +0900

    add py4e/ch09_dictionary.pyˆ

---
+++ py4e/ch09_dictionary.py
@@ -0,0 +1,15 @@
+ddd = dict()
+ddd['age'] = 21
+ddd['course'] = 182
+print(ddd)
+# {'age': 21, 'course': 182}
+
+ddd['age'] = 23
+print(ddd)
+# {'age': 23, 'course': 182}
+
+lst = list()
+lst.append(21)
+lst.append(183)
+print(lst)
+# [21, 183]
