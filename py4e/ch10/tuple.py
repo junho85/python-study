7ddaeaaee5fdcfcd82c97adb9e36f2a8e820c07a
june.kim <junho85@gmail.com>
2017-10-13 00:35:41 +0900

    add tuple.py

---
+++ py4e/ch10/tuple.py
@@ -0,0 +1,18 @@
+x , y = 3, 4
+print(y)
+
+x = { 'chuck' : 1 , 'fred' : 42, 'jan': 100}
+y = x.items()
+print(y)
+
+x = (5, 1, 3)
+if (3, 2, 0) > x:
+    print("test")
+
+# reverse list
+lst = [1,2,3]
+lst.sort(reverse=True)
+print(lst)
+
+days = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+print(days[2])
