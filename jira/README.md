a7f8c4653756e071af23f51f1bc1761abfda2371
june.kim <june.kim@kakaocorp.com>
2019-05-03 13:44:08 +0900

    jira sample

---
+++ jira/README.md
@@ -0,0 +1,1 @@
+config_sample.ini 를 config.ini 로 바꿔서 사용
--- jira/jira_test.py
+++ jira/jira_test.py
@@ -5,16 +5,13 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+url = config['DEFAULT']['URL']
 username = config['DEFAULT']['USERNAME']
 password = config['DEFAULT']['PASSWORD']
 
-print(username)
+jira = JIRA(url, auth=(username, password))
 
-jira = JIRA('https://jira.atlassian.com')
-# jira = JIRA('https://jira.atlassian.com', auth=(username, password))
-
-issue = jira.issue('JRA-9')
-
+issue = jira.issue('QAKAKAOMAIL-175')
 print(issue.fields.project.key)             # 'JRA'
 print(issue.fields.issuetype.name)          # 'New Feature'
 print(issue.fields.reporter.displayName)    # 'Mike Cannon-Brookes [Atlassian]'
