01f38d811f7abfedfc51f86b5d4fea99abac34e4
junho85 <junho85@gmail.com>
2019-11-11 01:05:47 +0900

    base64 decode example

--- base64/base64_example.py
+++ base64/base64_example.py
@@ -5,3 +5,10 @@ print(encoded) # 7JWI64WV7ZWY7IS47JqU
 
 decoded = base64.b64decode('7JWI64WV7ZWY7IS47JqU').decode('utf-8')
 print(decoded) # 안녕하세요
+
+encoded = base64.b64encode('데스크탑_MAC_Address.txt'.encode('utf-8'))
+print(encoded) # 642w7Iqk7YGs7YORX01BQ19BZGRyZXNzLnR4dA==
+
+encoded = base64.b64encode('데스크탑_MAC_Address.txt'.encode('euc-kr'))
+print(encoded) # taW9usWpxb5fTUFDX0FkZHJlc3MudHh0
+print(base64.b64decode('taW9usWpxb5fTUFDX0FkZHJlc3MudHh0').decode('euc-kr')) # 데스크탑_MAC_Address.txt
