966c01283e5afa816df5e90f1a7ad42ad27dda16
junho85 <junho85@gmail.com>
2019-11-11 17:59:27 +0900

    f_strings

---
+++ string/format/f_strings.py
@@ -0,0 +1,9 @@
+# python 3.6 의 새 기능. f-strings
+# https://docs.python.org/3/whatsnew/3.6.html
+
+a = "hello"
+b = "world"
+
+print(f'{a} {b}') # hello world
+
+print(f'{1} + {1} = {1 + 1}') # 1 + 1 = 2
