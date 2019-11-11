d20157ee0cb5016f952d5e71e84687654730345f
junho85 <junho85@gmail.com>
2019-11-11 17:59:15 +0900

    random test

---
+++ random/random_test.py
@@ -0,0 +1,10 @@
+import random
+import pprint
+
+# print(random.seed(1))
+print(random.random())
+print(random.random())
+print(random.random())
+print(random.random())
+print(random.random())
+print(random.uniform(1, 10))
